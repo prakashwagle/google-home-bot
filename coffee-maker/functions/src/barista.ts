c490fb651e1e817e0fb6dcc8188891cd2ddc8ce5
Prakash <prakashwagle@live.in>
2020-06-07 23:00:09 -0400

    Add error handling

--- coffee-maker/functions/src/barista.ts
+++ coffee-maker/functions/src/barista.ts
@@ -5,7 +5,7 @@ export function recommend(brew?:string):string{
    response = "Your brew of type "+brew+" requires 4min of brewing :)"
  }
  else{
-   throw "Undefined Brew Type";
+   throw new Error("Undefined Brew Type");
  }
   return response;
 }
