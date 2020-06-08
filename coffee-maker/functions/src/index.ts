2484fa127ee14832196b9136f4a7c0622650a3c6
Prakash <prakashwagle@live.in>
2020-06-07 22:09:35 -0400

    add test function

--- coffee-maker/functions/src/index.ts
+++ coffee-maker/functions/src/index.ts
@@ -1,8 +1,10 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  const value = request.query.name;
+  const msg:string = "Hello from Prakash Wagle Bitch! : ".concat(String(value).toUpperCase());
+ response.send(msg);
+});
